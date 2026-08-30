2dd59e2b7ca1bbd8e166b5bc8922b9a2f5c4ad77
fernandalouize <fernandalouizedomingues@gmail.com>
2026-08-30 17:04:07 -0300

    Initialize Spring Boot project with /health

---
+++ src/main/kotlin/br/ufpr/fernanda/delivery/HealthController.kt
@@ -0,0 +1,12 @@
+package br.ufpr.fernanda.delivery
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+	
+	@GetMapping("/health")
+	fun health(): String = "healthy! :-)"
+
+}
