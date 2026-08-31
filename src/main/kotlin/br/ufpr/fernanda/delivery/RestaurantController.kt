ff33668d13f5001eb3e13ed6f408708880b3a1f0
fernandalouize <fernandalouizedomingues@gmail.com>
2026-08-31 15:14:09 -0300

    Add restaurant listing and lookup

--- src/main/kotlin/br/ufpr/fernanda/delivery/RestaurantController.kt
+++ src/main/kotlin/br/ufpr/fernanda/delivery/RestaurantController.kt
@@ -3,8 +3,9 @@ package br.ufpr.fernanda.delivery
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-
-
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.http.ResponseEntity
 
 @RestController
 class RestaurantController(
@@ -12,4 +13,15 @@ class RestaurantController(
 ) {
 		@PostMapping("/restaurants")
 		fun create(@RequestBody restaurant: Restaurant): Restaurant = repository.save(restaurant)
+		@GetMapping("/restaurants")
+		fun list() : List<Restaurant> = repository.findAll()
+		@GetMapping("/restaurants/{id}")
+		fun findById(@PathVariable id: Long): ResponseEntity<Restaurant> {
+			val restaurant = repository.findById(id).orElse(null)
+			return if (restaurant == null){
+				ResponseEntity.notFound().build()
+			} else {
+				ResponseEntity.ok(restaurant)
+			}
+		}
 }
